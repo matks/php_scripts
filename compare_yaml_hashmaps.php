1e111416e27a2e3f53c466ab8d59645287418c66
Mathieu Ferment <mathieu.ferment@gmail.com>
2017-07-26 15:04:47 +0200

    Enhance compare_yaml_hashmaps.php

--- compare_yaml_hashmaps.php
+++ compare_yaml_hashmaps.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 use Symfony\Component\Yaml\Yaml;
 
@@ -60,29 +60,47 @@ $list2 = extractArrayFromFile(realpath($filepath2), $rootNode);
 
 $inFile1ButNotInFile2 = [];
 $inFile2ButNotInFile1 = [];
+$sameKeyDifferentValues = [];
 
 foreach ($list1 as $key => $value) {
     if (!array_key_exists($key, $list2)) {
         $inFile1ButNotInFile2[$key] = $value;
+    } elseif ($list1[$key] !== $list2[$key]) {
+        $sameKeyDifferentValues[$key] = $list1[$key] . ' / ' . $list2[$key];
     }
 }
 foreach ($list2 as $key => $value) {
     if (!array_key_exists($key, $list1)) {
         $inFile2ButNotInFile1[$key] = $value;
+    } elseif ($list2[$key] !== $list1[$key]) {
+        $sameKeyDifferentValues[$key] = $list1[$key] . ' / ' . $list2[$key];
     }
 }
 
 
 // echo result
 echo TextColorWriter::textColor('Done', TextColorWriter::BASH_PROMPT_GREEN) . PHP_EOL;
-echo TextColorWriter::textColor('Found in file 1 but not in file 2:', TextColorWriter::BASH_PROMPT_GREEN) . PHP_EOL;
 
-foreach ($inFile1ButNotInFile2 as $key => $value) {
-    echo '   - ' . $key . PHP_EOL;
+if (false === empty($inFile1ButNotInFile2)) {
+    echo TextColorWriter::textColor('Found in file 1 but not in file 2:', TextColorWriter::BASH_PROMPT_GREEN) . PHP_EOL;
+
+    foreach ($inFile1ButNotInFile2 as $key => $value) {
+        echo '   - ' . $key . PHP_EOL;
+    }
 }
 
-echo TextColorWriter::textColor('Found in file 2 but not in file 1:', TextColorWriter::BASH_PROMPT_GREEN) . PHP_EOL;
+if (false === empty($inFile2ButNotInFile1)) {
+    echo TextColorWriter::textColor('Found in file 2 but not in file 1:', TextColorWriter::BASH_PROMPT_GREEN) . PHP_EOL;
 
-foreach ($inFile2ButNotInFile1 as $key => $value) {
-    echo '   - ' . $key . PHP_EOL;
+    foreach ($inFile2ButNotInFile1 as $key => $value) {
+        echo '   - ' . $key . PHP_EOL;
+    }
+}
+
+if (false === empty($sameKeyDifferentValues)) {
+    echo TextColorWriter::textColor('Key in both hashmaps but different value:', TextColorWriter::BASH_PROMPT_GREEN) . PHP_EOL;
+
+    foreach ($sameKeyDifferentValues as $key => $value) {
+        echo '   - ' . $key . ' : ' . $value . PHP_EOL;
+    }
 }
